ca9154658bbb942c6368f7483efbc2b3f93a2a71
DmytroLitvinov <d.g.litvinov@gmail.com>
2023-11-27 13:23:17 +0200

    feat: add knex base implementation

---
+++ service/person.js
@@ -0,0 +1,11 @@
+const personDAO = require('../dao/person');
+
+class PersonService {
+  createPerson(personDTO) {
+    const { firstName, lastName, email } = personDTO;
+    
+    return personDAO.createPerson(firstName, lastName, email);
+  }
+}
+
+module.exports = new PersonService();
