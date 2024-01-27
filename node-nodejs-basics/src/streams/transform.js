98a2f89dcb9f6161bcc68fd773728095c00c9beb
Dmytro Litvinov <dmytro_litvinov@epam.com>
2024-01-27 20:34:14 +0200

    feat: implement streams api

--- node-nodejs-basics/src/streams/transform.js
+++ node-nodejs-basics/src/streams/transform.js
@@ -1,5 +1,23 @@
+import { pipeline, Transform } from 'node:stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+	const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, `${String(chunk).split('').reverse().join('')}\n`);
+    },
+  });
+
+	pipeline(
+		stdin,
+		reverseStream,
+    stdout,
+		(err) => {
+      if (err) {
+        console.error('Pipeline failed.', err);
+      }
+    },
+	);
 };
 
 await transform();
--- node-nodejs-basics/src/streams/write.js
+++ node-nodejs-basics/src/streams/write.js
@@ -1,5 +1,32 @@
+import path from 'node:path';
+import { pipeline } from 'node:stream';
+import { stdin } from 'node:process';
+import { createWriteStream } from 'node:fs';
+
+import { FILES } from '../constants/path-file.constant.js';
+import { getPath, checkFileExists } from '../utils/index.js';
+import { CustomValidationError } from '../errors/custom-validation.error.js';
+
 const write = async () => {
-    // Write your code here 
+	const fileName = 'fileToWrite.txt';
+  const sourcePath = path.resolve(getPath(import.meta.url), FILES, fileName); 
+  const isSourceFileExists = await checkFileExists(sourcePath);
+
+  if (!isSourceFileExists) {
+    throw new CustomValidationError();
+  }
+
+	const sourceStream = createWriteStream(sourcePath);
+
+	pipeline(
+		stdin,
+		sourceStream,
+		(err) => {
+      if (err) {
+        console.error('Pipeline failed.', err);
+      }
+    },
+	);
 };
 
 await write();
