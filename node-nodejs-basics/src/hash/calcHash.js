21ebb516fd48fa405b33ff8ad44554f7b230ead4
Dmytro Litvinov <dmytro_litvinov@epam.com>
2024-01-27 20:11:54 +0200

    feat: implement calcHash method

--- node-nodejs-basics/src/hash/calcHash.js
+++ node-nodejs-basics/src/hash/calcHash.js
@@ -1,5 +1,36 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import path from 'node:path';
+import { pipeline } from 'node:stream';
+
+import { FILES } from '../constants/path-file.constant.js';
+import { getPath, checkFileExists } from '../utils/index.js';
+import { CustomValidationError } from '../errors/custom-validation.error.js';
+
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = 'fileToCalculateHashFor.txt';
+  const hash = createHash('sha256');
+  const sourcePath = path.resolve(getPath(import.meta.url), FILES, fileName); 
+  const isSourceFileExists = await checkFileExists(sourcePath);
+
+  if (!isSourceFileExists) {
+    throw new CustomValidationError();
+  }
+
+  const sourceStream = createReadStream(sourcePath);
+  
+  pipeline(
+    sourceStream,
+    hash.setEncoding('hex'),
+    stdout,
+    (err) => {
+      if (err) {
+        console.error('Pipeline failed.', err);
+      }
+    },
+  );
 };
 
 await calculateHash();
