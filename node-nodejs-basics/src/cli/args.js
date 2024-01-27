6f7b0c613066976e118b01cf2469f281c7e0c666
Dmytro Litvinov <dmytro_litvinov@epam.com>
2024-01-27 18:34:02 +0200

    feat: implement cli functionality

--- node-nodejs-basics/src/cli/args.js
+++ node-nodejs-basics/src/cli/args.js
@@ -1,5 +1,24 @@
 const parseArgs = () => {
-    // Write your code here 
+  const result = [];
+  const argumentList = process.argv.slice(2);
+  const listLength = argumentList.length;
+  let i = 0;
+
+  while (i <= listLength - 1) {
+    if (argumentList[i].startsWith('--')) {
+      if (!argumentList[i + 1] || argumentList[i + 1].startsWith('--')) {
+        result.push([argumentList[i], undefined]);
+      } else {
+        result.push([argumentList[i], argumentList[i + 1]]);
+      }
+    }
+
+    i++;
+  }
+
+  const mappedResult = result.map(([key, value]) => `${key} is ${value}`).join(', ');
+
+  console.log(mappedResult);
 };
 
 parseArgs();
--- node-nodejs-basics/src/cli/env.js
+++ node-nodejs-basics/src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const PREFIX = 'RSS_';
+  const argumentList = Object.entries(process.env);
+  const filteredArguments = argumentList.filter((entry) => entry[0].startsWith(PREFIX));
+
+  filteredArguments.forEach(([key, value]) => {
+    process.stdout.write(`${key} = ${value}; `);
+  });
 };
 
 parseEnv();
