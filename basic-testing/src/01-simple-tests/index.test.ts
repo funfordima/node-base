b0dc3a200e4512fe846a1ff47c6b8b279707a008
DmytroLitvinov <d.g.litvinov@gmail.com>
2024-02-06 16:58:34 +0200

    feat: update simple-test to cover more cases

--- basic-testing/src/01-simple-tests/index.test.ts
+++ basic-testing/src/01-simple-tests/index.test.ts
@@ -2,39 +2,52 @@
 import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
-  const a = 3;
+  const list = [-3, 0, 3];
   const b = 2;
   test('should add two numbers', () => {
-    const result = simpleCalculator({ a, b, action: Action.Add });
-    expect(result).toBe(5);
+    const expectedResult = [-1, 2, 5];
+    list.forEach((a, i) => {
+      const result = simpleCalculator({ a, b, action: Action.Add });
+      expect(result).toBe(expectedResult[i]);
+    });
   });
 
   test('should subtract two numbers', () => {
-    const result = simpleCalculator({ a, b, action: Action.Subtract });
-    expect(result).toBe(1);
+    const expectedResult = [-5, -2, 1];
+    list.forEach((a, i) => {
+      const result = simpleCalculator({ a, b, action: Action.Subtract });
+      expect(result).toBe(expectedResult[i]);
+    });
   });
 
   test('should multiply two numbers', () => {
-    const result = simpleCalculator({ a, b, action: Action.Multiply });
-    expect(result).toBe(6);
+    const expectedResult = [-6, 0, 6];
+    list.forEach((a, i) => {
+      const result = simpleCalculator({ a, b, action: Action.Multiply });
+      expect(result).toBe(expectedResult[i]);
+    });
   });
 
   test('should divide two numbers', () => {
-    const a = 6;
-    const result = simpleCalculator({ a, b, action: Action.Divide });
-    expect(result).toBe(3);
-
-    // const failedResult = simpleCalculator({ a, b: 0, action: Action.Divide });
-    // expect(failedResult).toBe(Infinity);
+    // Infinity?
+    const list = [-6, 0, 6];
+    const expectedResult = [-3, 0, 3];
+    list.forEach((a, i) => {
+      const result = simpleCalculator({ a, b, action: Action.Divide });
+      expect(result).toBe(expectedResult[i]);
+    });
   });
 
   test('should exponentiate two numbers', () => {
-    const result = simpleCalculator({ a, b, action: Action.Exponentiate });
-    expect(result).toBe(9);
+    const expectedResult = [9, 0, 9];
+    list.forEach((a, i) => {
+      const result = simpleCalculator({ a, b, action: Action.Exponentiate });
+      expect(result).toBe(expectedResult[i]);
+    });
   });
 
   test('should return null for invalid action', () => {
-    const result = simpleCalculator({ a, b, action: 'InvalidAction' });
+    const result = simpleCalculator({ a: 3, b, action: 'InvalidAction' });
     expect(result).toBe(null);
   });
 
